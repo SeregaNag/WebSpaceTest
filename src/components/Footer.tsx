6f8417726beaf0dda1c0e730c6d44cb59bed2dd3
SeregaNag <Serega_nag@tut.by>
2024-12-27 13:02:59 +0300

    Hotfixes for build

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import "./Footer.css";
 import Image from "next/image";
 
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./Modal.css";
 import Image from "next/image";
